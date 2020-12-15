cb17fe0fb15e14e5d7423923ad9ede4a16a72620
neet-no1 <no1.neet.professional@gmail.com>
2020-12-15 21:35:32 +0900

    Springサンプルプロジェクト作成

---
+++ src/main/java/com/neetprofessional/api/SpringSample01Application.java
@@ -0,0 +1,13 @@
+package com.neetprofessional.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSample01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSample01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/neetprofessional/api/SpringSample01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neetprofessional.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSample01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
